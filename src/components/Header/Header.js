0e821996ac350c6e246767f718843f6b3bb940de
Kioshi Okamoto Rojas <kjor29@gmail.com>
2021-06-12 21:27:41 -0500

    :dart: Improve: agregar hero section

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,7 +10,7 @@ const Header = () => (
 		<Div1>
 			<Link href="/">
 				<a style={{ display: 'flex', alignItems: 'center', color: 'white', marginBottom: '20px' }}>
-					<DiCssdeck size="3rem" /> <Span>Porfolio</Span>
+					<DiCssdeck size="3rem" /> <Span>Portafolio</Span>
 				</a>
 			</Link>
 		</Div1>
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,16 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+	<Section row nopadding>
+		<LeftSection>
+			<SectionTitle main center>
+				Bienvenido a <br />
+				mi portafolio personal
+			</SectionTitle>
+			<SectionText>Desarrollar software de calidad es mi objetivo</SectionText>
+			<Button onClick={() => (window.location = 'https://github.com/kioshiokamoto')}>Conoce que hago</Button>
+		</LeftSection>
+	</Section>
 );
 
 export default Hero;
