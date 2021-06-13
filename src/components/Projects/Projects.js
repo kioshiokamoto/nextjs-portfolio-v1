493674d21598d033420542db3ba0ae19a104e943
Kioshi Okamoto Rojas <kjor29@gmail.com>
2021-06-12 21:47:37 -0500

    :dart: Improve: agregar area de tecnologias

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,13 +1,50 @@
 import React from 'react';
 
-import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag, TagList, TitleContent, UtilityList, Img } from './ProjectsStyles';
+import {
+	BlogCard,
+	CardInfo,
+	ExternalLinks,
+	GridContainer,
+	HeaderThree,
+	Hr,
+	Tag,
+	TagList,
+	TitleContent,
+	UtilityList,
+	Img,
+} from './ProjectsStyles';
 import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
 import { projects } from '../../constants/constants';
 
 const Projects = () => (
-  <div>
-    Projects
-  </div>
+	<Section nopadding id="projects">
+		<SectionDivider />
+		<SectionTitle main>Proyectos</SectionTitle>
+		<GridContainer>
+			{projects.map(({ id, image, title, description, source = '', visit = '', tags }) => (
+				<BlogCard key={id}>
+					<Img src={image} />
+					<TitleContent>
+						<HeaderThree title>{title}</HeaderThree>
+						<Hr />
+					</TitleContent>
+					<CardInfo>{description}</CardInfo>
+					<div>
+						<TitleContent>Stack</TitleContent>
+						<TagList>
+							{tags.map((tag, i) => (
+								<Tag key={i}>{tag}</Tag>
+							))}
+						</TagList>
+					</div>
+					<UtilityList>
+						{visit && <ExternalLinks href={source}>Código</ExternalLinks>}
+						{source && <ExternalLinks href={visit}>Vista previa</ExternalLinks>}
+					</UtilityList>
+				</BlogCard>
+			))}
+		</GridContainer>
+	</Section>
 );
 
 export default Projects;
--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,12 +1,47 @@
 import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
+import { DiFirebase, DiReact, DiZend, DiNodejs } from 'react-icons/di';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
-const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+const Technologies = () => (
+	<Section id="tech">
+		<SectionDivider />
+		<br />
+		<SectionTitle>Tecnologías</SectionTitle>
+		<SectionText>Me especializo en desarrollo web, actualmente veo backend y frontend</SectionText>
+		<List>
+			<ListItem>
+				<DiReact size="3rem" />
+				<ListContainer>
+					<ListTitle>Front-end</ListTitle>
+					<ListParagraph>
+						Experiencia con <br />
+						React.js
+					</ListParagraph>
+				</ListContainer>
+			</ListItem>
+			<ListItem>
+				<DiNodejs size="3rem" />
+				<ListContainer>
+					<ListTitle>Back-end</ListTitle>
+					<ListParagraph>
+						Experiencia con <br />
+						Node.js
+					</ListParagraph>
+				</ListContainer>
+			</ListItem>
+			<ListItem>
+				<DiFirebase size="3rem" />
+				<ListContainer>
+					<ListTitle>Bases de datos</ListTitle>
+					<ListParagraph>
+						Experiencia con <br />
+						MySql, MSQL y Postgres
+					</ListParagraph>
+				</ListContainer>
+			</ListItem>
+		</List>
+	</Section>
 );
 
 export default Technologies;
