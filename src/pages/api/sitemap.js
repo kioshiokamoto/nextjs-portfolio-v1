605ece3e914f4ee6d48d53ae4dcae4d32e708e8d
Kioshi Okamoto Rojas <kjor29@gmail.com>
2021-08-16 10:49:00 -0500

    :dart: Improve: agregar robots.txt y api

---
+++ src/pages/api/sitemap.js
@@ -0,0 +1,19 @@
+const {SitemapStream, streamToPromise} = require("sitemap")
+const {Readable} = require("stream")
+
+export default async (req,res) =>{
+    const links = [
+        {url:"/",changefreq:"daily",priority:0.9}
+    ]
+
+    const stream =new SitemapStream({hostname:`https://${req.headers.host}`});
+
+    res.writeHead(200,{
+        "Content-Type":"application/xml"
+    })
+    const xmlString = await streamToPromise(
+        Readable.from(links).pipe(stream)
+    ).then((data)=>data.toString())
+
+    res.end(xmlString)
+}
